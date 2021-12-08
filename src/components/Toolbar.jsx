8dab051029014a3e6b2953ccb30946173cb808d4
turboBarbitura <turbobarbitura@mail.ru>
2021-12-08 15:29:02 +0700

    add icons

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../styles/toolbar.scss'
-import {Brush} from './assets/img/brush.png'
+
 
 
 const Toolbar = () => {
     return (
         <div className='toolbar'>
+
             <button className='toolbar__btn brush'></button>
+            <button className='toolbar__btn rect'></button>
+            <button className='toolbar__btn circle'></button>
+            <button className='toolbar__btn eraser'></button>
+            <button className='toolbar__btn line'></button>
+            <button className='toolbar__btn undo'></button>
+            <button className='toolbar__btn redo'></button>
+            <button className='toolbar__btn save'></button>
 
         </div>
     );
