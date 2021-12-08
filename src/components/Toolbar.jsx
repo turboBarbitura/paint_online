f3abdc25b1e8402f371f33979ba5ee8e8ab2308d
turboBarbitura <turbobarbitura@mail.ru>
2021-12-08 11:37:24 +0700

    Start dev

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/toolbar.scss'
+import {Brush} from './assets/img/brush.png'
+
+
+const Toolbar = () => {
+    return (
+        <div className='toolbar'>
+            <button className='toolbar__btn brush'></button>
+
+        </div>
+    );
+};
+
+export default Toolbar;
