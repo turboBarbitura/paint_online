8641da3fc6993f26b53d69a851964ce3ce271b43
turboBarbitura <turbobarbitura@mail.ru>
2021-12-10 15:32:44 +0700

    Brush ADD

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../styles/toolbar.scss'
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
+import canvasState from "../store/canvasState";
 
 
 
@@ -7,11 +10,12 @@ const Toolbar = () => {
     return (
         <div className='toolbar'>
 
-            <button className='toolbar__btn brush'></button>
+            <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))} ></button>
             <button className='toolbar__btn rect'></button>
             <button className='toolbar__btn circle'></button>
             <button className='toolbar__btn eraser'></button>
             <button className='toolbar__btn line'></button>
+            <input style={{marginLeft:10}}  type="color"/>
             <button className='toolbar__btn undo'></button>
             <button className='toolbar__btn redo'></button>
             <button className='toolbar__btn save'></button>
